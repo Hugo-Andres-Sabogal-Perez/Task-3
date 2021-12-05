efe1754fa8fc1ff56096420f6a280d398344852d
Hugo-Andres-Sabogal-Perez <hugosabo2001@hotmail.com>
2021-12-05 16:28:51 -0500

    prueba

--- Task 3 respuesta.R
+++ Task 3 respuesta.R
@@ -1,17 +1,15 @@
-jknoinlknlkmn
+# Elaborado por: Tania Reina,Hugo Sabogal
+# Fecha de elaboracion: 5/12/2021
+# Ultima modificacion: 5/11/2021
+# Version de R: 4.1.1
 
 
-dasdas
 
-asdasd
+rm(list = ls())
+if(!require(pacman)) install.packages(pacman) 
+if(!require(pacman)) install.packages(pacman)
+require(pacman)
+p_load(tidyverse,rio,skimr,readxl,haven,WriteXLS)
+Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
 
-ads
-asd
-asdasdasd
 
-
-asd
-
-ads
-ads
-asdasd
